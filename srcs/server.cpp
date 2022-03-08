8e3a8e652d1f6cf4cd10a477195f17094ffb94ed
Océan <46847941+oc8@users.noreply.github.com>
2022-03-08 17:47:25 +0100

    .

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -13,7 +13,7 @@ Server::Server(int port, string password)
 Server::~Server() {}
 
 void Server::socket() {
-
+	master_socket = socket(
 }
 
 void Server::setsockopt() {
@@ -34,4 +34,50 @@ void Server::accept() {
 
 void Server::read() {
 
+}
+
+void Server::socket()
+{
+	if ((this->master_socket = ::socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		cerr << "socket failed" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Server::setsockopt()
+{
+	if (::setsockopt(this->master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&this->opt, sizeof(this->opt)) < 0)
+	{
+		cerr << "setsockopt" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Server::bind()
+{
+	if (::bind(this->master_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		cerr << "bind failed" << endl;
+		exit(EXIT_FAILURE);
+	}
+	cout << "Listener on port " << this->port << endl;
+}
+
+void Server::listen()
+{
+	if (::listen(this->master_socket, 3) < 0)
+	{
+		cerr << "listen" << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Server::accept()
+{
+	this->addrlen = sizeof(this->address);
+	if ((this->new_socket = ::accept(this->master_socket, (struct sockaddr *)&this->address, (socklen_t *)&this->addrlen)) < 0)
+	{
+		cerr << "accept" << endl;
+		exit(EXIT_FAILURE);
+	}
 }
